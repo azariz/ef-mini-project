cbb715d3eaabdbb4c474d210469df8f2046091b1
Amine Azariz <amine@azariz.com>
2021-08-15 19:31:33 +0100

    Initial work for receiving json with a POST http request.

--- app.py
+++ app.py
@@ -1,7 +1,48 @@
-from flask import Flask
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+# Author : Amine Azariz
+
+# * Imports
+# --------------------------------------------------------------------------- *
+from flask import Flask, request, jsonify
+
+# * Params
+# --------------------------------------------------------------------------- *
+
+
+# * Config
+# --------------------------------------------------------------------------- *
 app = Flask(__name__)
+app.DEBUG = True
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+# * Routes Views
+# --------------------------------------------------------------------------- *
+
+# Index view
+# --
+@app.route('/')
+def view_index():
+    return '<p>Hello, World!</p>'
+
+
+# Index view
+# --
+# @TODO: with more time, use Flask-RESTful instead
+@app.route('/events/', methods=['GET', 'POST'])
+def api_events():
+
+    # behaviour for POST request (adding an event)
+    if request.method == 'POST':
+        my_event = request.json
+        app.logger.info(f"Received JSON is : {my_event}")
+        return jsonify({'status':'ok'}), 200
+
+    # behaviour for GET request (requesting all events)
+    elif request.method == 'GET':
+        return jsonify({'status':'not implemented.'}), 501
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
