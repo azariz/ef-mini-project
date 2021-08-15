20fa88f52b55dc4f18992350142e90119351ca27
Amine Azariz <amine@azariz.com>
2021-08-15 20:24:12 +0100

    (Unstable) Implementation of a first attempt to create an event on ZOOM API. WARNING : the event is hard-coded for testing, not yet linked to the api upcoming JSON.

--- app.py
+++ app.py
@@ -5,17 +5,65 @@
 
 # * Imports
 # --------------------------------------------------------------------------- *
-from threading import local
 from flask import Flask, request, jsonify
+from time import time
+import jwt
+import requests
+import json
 
 # * Params
 # --------------------------------------------------------------------------- *
-
+ZOOM_API = 'https://api.zoom.us/v2/users/me/meetings'
+ZOOM_KEY = 'asl8P-7OQ2iY6-QjxzV70A'
+ZOOM_SECRET = 'oUWLZSxyxiPMAleDrgA4lMv13G4qFUoLgtfR'
+ZOOM_KEY_EXPIRY = 5000  # seconds
 
 # * Config
 # --------------------------------------------------------------------------- *
 app = Flask(__name__)
 app.DEBUG = True
+
+
+# * Utils
+# --------------------------------------------------------------------------- *
+
+# Zoom Get Token
+# generate a new JWT token for the ZOOM API
+def zoom_get_token():
+    token = jwt.encode(
+        {'iss': ZOOM_KEY, 'exp': time() + 5000},
+        ZOOM_SECRET,
+        algorithm='HS256'
+    )
+    return token
+
+
+# Zoom New Meeting
+# transform front data event to a meeting event
+def zoom_new_meeting(user_event):
+    event_data = {"topic": "The title of your zoom meeting",
+                  "type": 2,  # scheduled meeting
+                  "start_time": "2021-09-15T10:10:00",
+                  "duration": "30",  # minutes
+                  "timezone": "Europe/Paris"
+                  }
+    return event_data
+
+
+# Zoom Post Event
+# fetch zoom api and get returns
+def zoom_post_event(event_data):
+    # prepare request
+    headers = {'authorization': 'Bearer %s' % zoom_get_token(),
+               'content-type': 'application/json'}
+
+    # post to zoom api
+    zoom_request = requests.post(ZOOM_API, headers=headers,
+                                 data=json.dumps(event_data))
+
+    # jsonify the zoom response to a dict
+    zoom_response = json.loads(zoom_request.text)
+    return zoom_response
 
 
 # * Routes: Views
@@ -37,12 +85,19 @@ def view_index():
 @app.route('/events/', methods=['GET', 'POST'])
 def api_events():
 
-    # behaviour for POST request (adding an event)
     if request.method == 'POST':
-        my_event = request.json
-        app.logger.info(f"Received JSON is : {my_event}")
-        return jsonify({'status':'ok'}), 200
+        # behaviour for POST request (adding an event)
+        # parse data front front
+        user_event = request.json
+        app.logger.info(f"Received JSON is : {user_event}")
 
-    # behaviour for GET request (requesting all events)
+        # create a zoom meeting then post it to API
+        zoom_meeting_data = zoom_new_meeting(user_event)
+        res = zoom_post_event(zoom_meeting_data)
+
+        # if all good, return OK
+        return jsonify({'status': 'ok', 'zoom_res': res}), 200
+
     elif request.method == 'GET':
-        return jsonify({'status':'not implemented.'}), 501
+        # behaviour for GET request (requesting all events)
+        return jsonify({'status': 'not implemented.'}), 501
